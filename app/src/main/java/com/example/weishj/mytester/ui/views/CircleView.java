aba88afc889476ee5416ccd667e1feed0ceb1be1
jackiewei <shijiewei@users.noreply.github.com>
2018-11-15 22:11:11 +0800

    解决wrap_content问题

--- app/src/main/java/com/example/weishj/mytester/ui/views/CircleView.java
+++ app/src/main/java/com/example/weishj/mytester/ui/views/CircleView.java
@@ -8,6 +8,8 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class CircleView extends View {
+	private static final int DEFAULT_WIDTH = 200;
+	private static final int DEFAULT_HEIGHT = 200;
 	private int mColor = Color.RED;
 	private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
@@ -37,5 +39,23 @@ public class CircleView extends View {
 		int height = getHeight();
 		int radius = Math.min(width, height) / 2;
 		canvas.drawCircle(width / 2, height / 2, radius, mPaint);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+		// 重点1：通过以下代码解决自定义view的wrap_content不生效（等效于match_parent）的问题
+		int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+		int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+		int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+		int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+		if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST) {
+			setMeasuredDimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		} else if (widthSpecMode == MeasureSpec.AT_MOST) {
+			setMeasuredDimension(DEFAULT_WIDTH, heightSpecSize);
+		} else if (heightSpecMode == MeasureSpec.AT_MOST) {
+			setMeasuredDimension(widthSpecSize, DEFAULT_HEIGHT);
+		}
 	}
 }
